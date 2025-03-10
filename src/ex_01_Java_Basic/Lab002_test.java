c11f9e5ded08d3cf4520651e9aed974431dafa7a
dk1509tx <deepbhalavat@gmail.com>
2025-03-10 07:46:42 +0530

    10-Mar I am pushing test code

---
+++ src/ex_01_Java_Basic/Lab002_test.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab002_test {
+    public static void main(String[] args) {
+        System.out.println("test2");
+
+
+
+    }
+}
---
+++ src/ex_01_Java_Basic/LoginPage.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class LoginPage {
+    public static void main(String[] args) {
+        // First letter is capital and we use
+    }
+}
