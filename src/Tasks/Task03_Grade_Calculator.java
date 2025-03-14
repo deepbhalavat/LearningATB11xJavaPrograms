e6e264895e3db5130ec5bb0732cd2aa407db035b
dk1509tx <deepbhalavat@gmail.com>
2025-03-14 07:08:12 +0530

    13-Mar Task

--- src/Tasks/Task03_Grade_Calculator.java
+++ src/Tasks/Task03_Grade_Calculator.java
@@ -17,7 +17,7 @@ D: 60-69
 
 F: 0-59
          */
-        String Grade_Result = (grade <= 59) ? "F" : (grade <= 69 ? "D" : (grade <= 79 ? "C" : (grade <= 89 ? "B" : (grade <= 100 ? "A" : "Grade should be between 0 to 100"))));
+        String Grade_Result = (grade > 0) ? ((grade <= 59) ? ("F - "+grade) : (grade <= 69 ? ("D - "+grade) : (grade <= 79 ? ("C - "+grade) : (grade <= 89 ? ("B - "+grade) : (grade <= 100 ? ("A - "+grade) : "Grade should be between 0 to 100"))))) : ("Grade must be in Positive as entered value is " + grade);
         System.out.println(Grade_Result);
 
     }
